45d1bf9d6e1166d8bada79b1cc72572e780cee66
Guillaume Lostis <g.lostis@kayrros.com>
2019-12-04 08:56:28 +0100

    Add code for day04 part2

--- day04.py
+++ day04.py
@@ -1,4 +1,4 @@
-import os
+import re
 
 
 def part1(range_min, range_max):
@@ -6,6 +6,18 @@ def part1(range_min, range_max):
     for i in range(range_min, range_max + 1):
         if meets_criteria(i):
             count += 1
+    return count
+
+
+def part2(range_min, range_max):
+    count = 0
+    for i in range(range_min, range_max + 1):
+        if meets_criteria(i):
+            if meets_new_criteria(i):
+                count += 1
+            else:
+                # print(i)
+                pass
     return count
 
 
@@ -27,5 +39,18 @@ def meets_criteria(number):
     return answer
 
 
+def meets_new_criteria(number):
+    number = str(number)
+    for i in range(0, 10):
+        if (
+            re.match(fr"^{i}{i}[^{i}].*", number)
+            or re.match(fr".*[^{i}]{i}{i}$", number)
+            or re.match(fr".*[^{i}]{i}{i}[^{i}].*", number)
+        ):
+            return True
+    return False
+
+
 if __name__ == "__main__":
     print(part1(130254, 678275))
+    print(part2(130254, 678275))
