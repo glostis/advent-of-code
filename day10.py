fc2139feaa1a8a4232b96f96a852bfd26907e276
Guillaume Lostis <g.lostis@kayrros.com>
2019-12-10 18:37:47 +0100

    [WIP] Day10 part2

--- day10.py
+++ day10.py
@@ -18,15 +18,23 @@ def part1(lines):
                 angles.add(np.arctan2(asteroid[0] - other[0], asteroid[1] - other[1]))
         asteroids[asteroid] = angles
 
+    max_k = None
     max_v = 0
-    for v in asteroids.values():
+    for k, v in asteroids.items():
         if len(v) > max_v:
+            max_k = k
             max_v = len(v)
 
-    return max_v
+    return max_k, max_v, asteroids
+
+
+def part2(asteroids, position):
+    print(position)
 
 
 if __name__ == "__main__":
     with open(os.path.join("data", "day10_1.txt")) as f:
         lines = [el.strip() for el in f]
-        print(part1(lines))
+        position, neighbors, asteroids = part1(lines)
+        print(neighbors)
+        part2(asteroids, position)
