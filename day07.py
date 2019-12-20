5a129efda024f3ea53b3a3ec90329fe165bec44d
Guillaume Lostis <g.lostis@kayrros.com>
2019-12-20 21:37:29 +0100

    Use intocde computer for days 07 and 11

--- day07.py
+++ day07.py
@@ -1,196 +1,56 @@
 import itertools
 import os
 
+from intcode import Intcode
 
-def part1(_codes, verbose=False):
+
+def part1(program):
     max_output = 0
     for phases in itertools.permutations(range(5)):
         input_signal = 0
         for phase in phases:
-            output = intcode_computer(_codes, phase, input_signal, verbose=verbose)
-            input_signal = output
-        if output > max_output:
-            max_output = output
-    return max_output
-
-
-class OpCode99(Exception):
-    pass
-
-
-def part2(_codes, verbose=False):
-    max_output = 0
-    for phases in itertools.permutations(range(5, 10)):
-        input_signal = 0
-        # Initialize amplifiers with their respective phase settings
-        states = dict()
-        for phase in phases:
-            output, __codes, point, input_counter = intcode_computer(
-                _codes, phase, input_signal, return_on_code_4=True, verbose=verbose
-            )
-            states[phase] = (__codes.copy(), point, input_counter)
-            input_signal = output
-        i = 0
-        while True:
-            __codes, point, input_counter = states[phases[i]]
-            try:
-                o = intcode_computer(
-                    __codes,
-                    input_signal=input_signal,
-                    point=point,
-                    input_counter=input_counter,
-                    return_on_code_4=True,
-                    verbose=verbose,
-                )
-            except OpCode99:
-                break
-            input_signal, __codes, point, input_counter = o
-            states[phases[i]] = (__codes, point, input_counter)
-            i += 1
-            if i == 5:
-                i = 0
+            computer = Intcode(program)
+            computer.inputs = [phase, input_signal]
+            computer.run_til_output()
+            input_signal = computer.outputs[0]
         if input_signal > max_output:
             max_output = input_signal
     return max_output
 
 
-def parse_instruction(codes, point):
-    instruction = codes[point]
+def part2(program):
+    max_output = 0
+    for phases in itertools.permutations(range(5, 10)):
+        input_signal = 0
+        # Initialize amplifiers with their respective phase settings
+        computers = []
+        for phase in phases:
+            computer = Intcode(program)
+            computer.inputs.append(phase)
+            computers.append(computer)
 
-    # Left-pad instruction with zeros to make it of length 4
-    instruction = f"{instruction:05}"
+        i = 0
+        while True:
+            computer = computers[i]
+            computer.inputs.append(input_signal)
+            computer.run_til_output()
+            if computer.opcode == 99:
+                break
+            input_signal = computer.outputs.pop()
+            i += 1
+            i = i % len(phases)
 
-    # Parse opcode
-    opcode = int(instruction[-2:])
-
-    # Parse operand modes, and reverse them (so that they are read from left to right)
-    operand_modes = instruction[:-2][::-1]
-
-    if opcode in [1, 2, 5, 6, 7, 8]:
-        nb_params = 2
-    elif opcode in [4]:
-        nb_params = 1
-    else:
-        # Unknown opcode, or halting code 99
-        nb_params = 0
-
-    operands = []
-    for i in range(nb_params):
-        operand = codes[point + i + 1]
-        if operand_modes[i] == "0":
-            # Address mode
-            operands.append(codes[operand])
-        elif operand_modes[i] == "1":
-            # Immediate mode
-            operands.append(operand)
-        else:
-            raise ValueError(f"Unknown operation mode {operand_modes[i]}")
-
-    if opcode in [1, 2, 7, 8]:
-        result_address = codes[point + 3]
-        if operand_modes[2] != "0":
-            raise ValueError(
-                "Parameters that an instruction writes to will never be in immediate mode."
-            )
-    elif opcode in [3]:
-        result_address = codes[point + 1]
-        if operand_modes[0] != "0":
-            raise ValueError(
-                "Parameters that an instruction writes to will never be in immediate mode."
-            )
-    else:
-        result_address = None
-
-    return opcode, instruction, operands, result_address
-
-
-def intcode_computer(
-    _codes,
-    phase_setting=None,
-    input_signal=None,
-    point=0,
-    input_counter=0,
-    return_on_code_4=False,
-    verbose=False,
-):
-    codes = _codes.copy()
-    while True:
-        opcode, instruction, operands, result_address = parse_instruction(codes, point)
-        if verbose:
-            print(f"Opcode {opcode}")
-
-        if opcode in [1, 2]:
-            skip = 4
-            if opcode == 1:
-                operation_result = operands[0] + operands[1]
-            else:
-                operation_result = operands[0] * operands[1]
-
-            codes[result_address] = operation_result
-            if verbose:
-                print(f"Storing {operation_result} to address {result_address}")
-            point += skip
-            if verbose:
-                print(f"Skipping {skip} to {point}")
-
-        elif opcode in [3, 4]:
-            skip = 2
-            if opcode == 3:
-                if input_counter == 0:
-                    _input = phase_setting
-                else:
-                    _input = input_signal
-                input_counter += 1
-                if verbose:
-                    print(f"Storing input {_input} to address {result_address}")
-                codes[result_address] = _input
-            else:
-                output = operands[0]
-                if verbose:
-                    print(f"Output: {output}")
-                if return_on_code_4:
-                    point += skip
-                    return (output, codes, point, input_counter)
-            point += skip
-            if verbose:
-                print(f"Skipping {skip} to {point}")
-
-        elif opcode in [5, 6]:
-            if opcode == 5:
-                cond = operands[0] != 0
-            else:
-                cond = operands[0] == 0
-
-            if cond:
-                point = operands[1]
-            else:
-                point += 3
-
-        elif opcode in [7, 8]:
-            skip = 4
-            if opcode == 7:
-                res = 1 if operands[0] < operands[1] else 0
-            else:
-                res = 1 if operands[0] == operands[1] else 0
-
-            codes[result_address] = res
-            point += skip
-            if verbose:
-                print(f"Skipping {skip} to {point}")
-
-        elif opcode == 99:
-            if return_on_code_4:
-                raise OpCode99("Opcode 99 was reached")
-            else:
-                return output
-        else:
-            raise ValueError(f"Unknown opcode {opcode}")
-    if verbose:
-        print()
+        if input_signal > max_output:
+            max_output = input_signal
+    return max_output
 
 
 if __name__ == "__main__":
     with open(os.path.join("data", "day07_1.txt")) as f:
         codes = [int(code) for code in f.read().split(",")]
-        print(part1(codes))
-        print(part2(codes))
+        ret = part1(codes)
+        assert ret == 929800
+        print("part1 day07 OK")
+        ret = part2(codes)
+        assert ret == 15432220
+        print("part2 day07 OK")
--- day11.py
+++ day11.py
@@ -4,14 +4,16 @@ from collections import defaultdict
 
 import numpy as np
 
+from intcode import Intcode
 
-def part1(_codes, verbose=False):
-    painted, _ = paint_panels(_codes, verbose=verbose)
+
+def part1(program):
+    painted, _ = paint_panels(program)
     return painted
 
 
-def part2(_codes, verbose=False):
-    _, panels = paint_panels(_codes, starting_color=1, verbose=verbose)
+def part2(program):
+    _, panels = paint_panels(program, starting_color=1)
 
     positions = np.array(list(panels.keys()))
     xmin, ymin = np.min(positions, axis=0)
@@ -30,7 +32,7 @@ def part2(_codes, verbose=False):
         print(st)
 
 
-def paint_panels(_codes, starting_color=0, verbose=False):
+def paint_panels(program, starting_color=0):
     # Stores panels painted at least once
     painted = set()
 
@@ -46,266 +48,59 @@ def paint_panels(_codes, starting_color=0, verbose=False):
     # Stores the direction in which the robot is facing
     heading = 0
 
-    point = 0
-    relative_base = 0
+    computer = Intcode(program)
     while True:
-        try:
-            # Get the color of the current panel
-            inp = panels[(x, y)]
+        # Get the color of the current panel
+        inp = panels[(x, y)]
 
-            # Run the intcode computer once to get the color that should be painted
-            color, _codes, point, counter, relative_base = intcode_computer(
-                _codes,
-                phase_setting=inp,
-                point=point,
-                relative_base=relative_base,
-                return_on_code_4=True,
-                verbose=verbose,
-            )
-            assert color in [0, 1]
-
-            # Paint the panel
-            panels[(x, y)] = color
-
-            # Store panel in painted panels
-            painted.add((x, y))
-
-        except OpCode99:
+        # Run the intcode computer once to get the color that should be painted
+        computer.inputs.append(inp)
+        computer.run_til_output()
+        if computer.opcode == 99:
             break
+        color = computer.outputs.pop()
+        assert color in [0, 1]
 
-        try:
-            # Run the intcode computer again to get the new robot's heading
-            heading_code, _codes, point, counter, relative_base = intcode_computer(
-                _codes,
-                phase_setting=None,
-                point=point,
-                relative_base=relative_base,
-                return_on_code_4=True,
-                verbose=verbose,
-            )
-            assert heading_code in [0, 1]
+        # Paint the panel
+        panels[(x, y)] = color
 
-            # Turn the robot
-            if heading_code == 0:
-                heading -= 90
-            else:
-                heading += 90
-            heading = heading % 360
+        # Store panel in painted panels
+        painted.add((x, y))
 
-            # Move the robot forward
-            if heading == 0:
-                y += 1
-            elif heading == 90:
-                x += 1
-            elif heading == 180:
-                y -= 1
-            elif heading == 270:
-                x -= 1
-            else:
-                raise ValueError(f"Heading {heading}")
-
-        except OpCode99:
+        # Run the intcode computer again to get the new robot's heading
+        computer.run_til_output()
+        if computer.opcode == 99:
             break
+        heading_code = computer.outputs.pop()
+        assert heading_code in [0, 1]
+
+        # Turn the robot
+        if heading_code == 0:
+            heading -= 90
+        else:
+            heading += 90
+        heading = heading % 360
+
+        # Move the robot forward
+        if heading == 0:
+            y += 1
+        elif heading == 90:
+            x += 1
+        elif heading == 180:
+            y -= 1
+        elif heading == 270:
+            x -= 1
+        else:
+            raise ValueError(f"Heading {heading}")
 
     return painted, panels
-
-
-class OpCode99(Exception):
-    pass
-
-
-class DynamicList(list):
-    def __setitem__(self, key, value):
-        if key >= len(self):
-            for _ in range(len(self), key + 1):
-                self.append(0)
-        return super(DynamicList, self).__setitem__(key, value)
-
-    def __getitem__(self, key):
-        if key >= len(self):
-            for _ in range(len(self), key + 1):
-                self.append(0)
-        return super(DynamicList, self).__getitem__(key)
-
-
-def parse_instruction(codes, point, relative_base):
-    instruction = codes[point]
-
-    # Left-pad instruction with zeros to make it of length 4
-    instruction = f"{instruction:05}"
-
-    # Parse opcode
-    opcode = int(instruction[-2:])
-
-    # Parse operand modes, and reverse them (so that they are read from left to right)
-    operand_modes = instruction[:-2][::-1]
-
-    if opcode in [1, 2, 5, 6, 7, 8]:
-        nb_params = 2
-    elif opcode in [4, 9]:
-        nb_params = 1
-    else:
-        # Unknown opcode, or halting code 99
-        nb_params = 0
-
-    operands = []
-    for i in range(nb_params):
-        operand = codes[point + i + 1]
-        if operand_modes[i] == "0":
-            # Position mode
-            operands.append(codes[operand])
-        elif operand_modes[i] == "1":
-            # Immediate mode
-            operands.append(operand)
-        elif operand_modes[i] == "2":
-            # Relative mode
-            operands.append(codes[operand + relative_base])
-        else:
-            raise ValueError(f"Unknown operation mode {operand_modes[i]}")
-
-    if opcode in [1, 2, 7, 8]:
-        result_pointer = point + 3
-        mode = operand_modes[2]
-    elif opcode in [3]:
-        result_pointer = point + 1
-        mode = operand_modes[0]
-
-    if opcode in [1, 2, 3, 7, 8]:
-        if mode == "0":
-            result_address = codes[result_pointer]
-        elif mode == "1":
-            raise ValueError(
-                "Parameters that an instruction writes to will never be in immediate mode."
-            )
-        elif mode == "2":
-            result_address = codes[result_pointer] + relative_base
-    else:
-        result_address = None
-
-    return opcode, operands, result_address
-
-
-OPCODES = {
-    1: "Add",
-    2: "Multiply",
-    3: "Input",
-    4: "Output",
-    5: "Jump-if-true",
-    6: "Jump-if-false",
-    7: "Less-than",
-    8: "Equals",
-    9: "Change-base",
-}
-
-
-def intcode_computer(
-    _codes,
-    phase_setting=None,
-    input_signal=None,
-    point=0,
-    input_counter=0,
-    relative_base=0,
-    return_on_code_4=False,
-    verbose=False,
-):
-    codes = DynamicList(_codes)
-    while True:
-        opcode, operands, result_address = parse_instruction(codes, point, relative_base)
-        if verbose:
-            print()
-            print(f"Opcode {opcode} - {OPCODES.get(opcode)}")
-
-        if opcode in [1, 2]:
-            skip = 4
-            if opcode == 1:
-                operation_result = operands[0] + operands[1]
-            else:
-                operation_result = operands[0] * operands[1]
-
-            codes[result_address] = operation_result
-            if verbose:
-                print(f"Storing {operation_result} to address {result_address}")
-            point += skip
-            if verbose:
-                print(f"Skipping {skip} to {point}")
-
-        elif opcode in [3, 4]:
-            skip = 2
-            if opcode == 3:
-                if input_counter == 0:
-                    _input = phase_setting
-                else:
-                    _input = input_signal
-                input_counter += 1
-                if verbose:
-                    print(f"Storing input {_input} to address {result_address}")
-                codes[result_address] = _input
-            else:
-                output = operands[0]
-                if verbose:
-                    print(f"Output: {output}")
-                if return_on_code_4:
-                    point += skip
-                    return (output, codes, point, input_counter, relative_base)
-            point += skip
-            if verbose:
-                print(f"Skipping {skip} to {point}")
-
-        elif opcode in [5, 6]:
-            if opcode == 5:
-                cond = operands[0] != 0
-            else:
-                cond = operands[0] == 0
-
-            if verbose:
-                print(f"Condition is {cond}")
-
-            if cond:
-                point = operands[1]
-            else:
-                point += 3
-            if verbose:
-                print(f"Setting pointer to {point}")
-
-        elif opcode in [7, 8]:
-            skip = 4
-            if opcode == 7:
-                res = 1 if operands[0] < operands[1] else 0
-            else:
-                res = 1 if operands[0] == operands[1] else 0
-
-            codes[result_address] = res
-
-            if verbose:
-                print(f"Storing result {res} at address {result_address}")
-
-            point += skip
-            if verbose:
-                print(f"Skipping {skip} to {point}")
-
-        elif opcode in [9]:
-            skip = 2
-            relative_base += operands[0]
-
-            if verbose:
-                print(f"Setting relative_base to {relative_base}")
-
-            point += skip
-
-        elif opcode == 99:
-            if return_on_code_4:
-                raise OpCode99("Opcode 99 was reached")
-            else:
-                return output
-        else:
-            raise ValueError(f"Unknown opcode {opcode}")
-    if verbose:
-        print()
 
 
 if __name__ == "__main__":
     with open(os.path.join("data", "day11_1.txt")) as f:
         codes = [int(code) for code in f.read().split(",")]
         painted = part1(codes)
-        print(len(painted))
+        assert len(painted) == 2255
+        print("part1 day11 OK")
         part2(codes)
+        print("part2 day11 OK")
