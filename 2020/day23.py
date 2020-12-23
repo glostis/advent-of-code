a0ffae42468734fdae661701593f5408bb4d65e3
Guillaume Lostis <g.lostis@kayrros.com>
2020-12-23 15:04:58 +0100

    Day 23 Part 2 2020

--- 2020/day23.py
+++ 2020/day23.py
@@ -1,31 +1,59 @@
-from collections import deque
+from tqdm import tqdm
 
 
-def play_round(cups, length=9):
-    current_cup = cups.popleft()
-    next_cups = tuple(cups.popleft() for _ in range(3))
+def play_round(cups, current_cup, length=9):
+    next_cups = []
+    for _ in range(3):
+        next_cup = cups[current_cup]
+        next_cups.append(next_cup)
+        cups[current_cup] = cups.pop(next_cup)
 
     destination_cup = ((current_cup - 1) - 1) % length + 1
     while destination_cup in next_cups:
         destination_cup = ((destination_cup - 1) - 1) % length + 1
 
-    cups.appendleft(current_cup)
-
-    insertion_point = cups.index(destination_cup) + 1
     for cup in reversed(next_cups):
-        cups.insert(insertion_point, cup)
+        link = cups[destination_cup]
+        cups[destination_cup] = cup
+        cups[cup] = link
 
-    cups.rotate(-1)
-    return cups
+    return cups[current_cup]
+
+
+def part1(cups, current_cup, length):
+    for _ in range(100):
+        current_cup = play_round(cups, current_cup=current_cup, length=length)
+
+    pointer = 1
+    res = ""
+    for _ in range(8):
+        res += str(cups[pointer])
+        pointer = cups[pointer]
+    return res
+
+
+def part2(cups, current_cup, length):
+    for _ in tqdm(range(10000000)):
+        current_cup = play_round(cups, current_cup=current_cup, length=length)
+
+    res = cups[1] * cups[cups[1]]
+    return res
 
 
 def main():
-    cups = deque(map(int, "123487596"))
-    length = len(cups)
-    for _ in range(100):
-        play_round(cups, length)
-    res = "".join(map(str, [cups[(cups.index(1) + 1 + i) % len(cups)] for i in range(8)]))
-    print(int(res))
+    l = list(map(int, "123487596"))
+    current_cup = l[0]
+    length = len(l)
+    cups = dict()
+    for i, c in enumerate(l):
+        cups[c] = l[(i + 1) % length]
+    print(part1(cups, current_cup, length))
+    l.extend(range(10, 1000001))
+    length = len(l)
+    cups = dict()
+    for i, c in enumerate(l):
+        cups[c] = l[(i + 1) % length]
+    print(part2(cups, current_cup, length))
 
 
 if __name__ == "__main__":
